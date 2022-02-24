e16b7324117a5e174e8343e8ef3be35f234cda46
Emilio De Lazzari <emilio@Admins-MacBook-Pro.local>
2022-02-24 13:59:30 +0100

    Removed node_modules

--- server/dist/config.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.components = void 0;
-exports.components = {
-    ws: null,
-    redis: null
-};
-//# sourceMappingURL=config.js.map
--- server/dist/publicRoutes/login.js
+++
@@ -1,5 +0,0 @@
-// import axios, { AxiosResponse } from 'axios'
-// import { v4 } from 'uuid'
-// import { Router, Request, Response } from 'express';
-// export const setupLogin = (publicRoute: any, components: any) => {}
-//# sourceMappingURL=login.js.map
